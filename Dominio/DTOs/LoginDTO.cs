1e6ca3fbbc81d280a53d8a33b68a0456cceb7552
Jonas <jonas.pereira.lima1@gmail.com>
2024-10-11 00:33:06 -0300

    iniciando projeto

---
+++ Dominio/DTOs/LoginDTO.cs
@@ -0,0 +1,10 @@
+
+namespace Minimal_Api.Dominio.DTOs
+{
+    public class LoginDTO
+    {
+        public string Email { get; set; } = default!;
+        public string Password { get; set; } = default!;
+    }
+}
+
